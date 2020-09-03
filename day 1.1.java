ed9c023809e2263812dff7321d888859400d6da7
Parimal <47852029+parimal-donga@users.noreply.github.com>
2020-09-03 19:35:32 +0530

    Update day 1.1.java

--- day 1.1.java
+++ day 1.1.java
@@ -2,7 +2,7 @@ class Solution {
     public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1])return i;
+            if(nums[i]==nums[i-1])return nums[i];
         }
         return 0;
     }
