f5834999171bef78fd4a9af8e1fd421a56214e9e
Parimal <47852029+parimal-donga@users.noreply.github.com>
2020-09-05 16:18:33 +0530

    Update 3. Majority Element (>N2 times).java
    https://leetcode.com/problems/majority-element/submissions/

--- day3/3. Majority Element (>N2 times).java
+++ day3/3. Majority Element (>N2 times).java
@@ -4,3 +4,18 @@ class Solution {
         return nums[nums.length/2];
     }
 }
+/*
+//boyer moore algo
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        Integer kk= null;
+        for(int ok:nums){
+            if(count==0)kk=ok;
+            count+=(ok==kk)?1:-1;
+        }
+        return kk;
+    }
+}
+
+*/
